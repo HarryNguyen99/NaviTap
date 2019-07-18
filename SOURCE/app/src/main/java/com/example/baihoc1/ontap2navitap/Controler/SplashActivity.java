98fadee1309aac98a6c8f8bb9b003394d4fc6f8c
Harry Nguyen <phuquyit@gmail.com>
2019-07-18 14:23:32 +0700

    login v3

---
+++ SOURCE/app/src/main/java/com/example/baihoc1/ontap2navitap/Controler/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.baihoc1.ontap2navitap.Controler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.baihoc1.ontap2navitap.R;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
